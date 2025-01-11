b1720f32074dcfae79bdcfa8f6c4e51b79c5266e
thebeast01 <mohammadsaif0847@gmail.com>
2025-01-11 21:09:57 +0530

    Fixed Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,7 +4,7 @@ export const Navbar = () => {
   const navigate = useNavigate()
   return (
     <>
-      <nav className="bg-gray-800 p-4">
+      <nav className="bg-gray-800 p-4 min-w-[585px]">
         <ul className="flex justify-between">
           <li>
             <h1 className="text-slate-50 text-3xl italic ">Travel World</h1>
